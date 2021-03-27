ab37e5fb7e82498c5aa7062ca1bc6e574b465bc1
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-27 13:35:08 +0530

    Added: Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,12 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 attendance=$(($RANDOM%2))
-
+wagePerHr=20
+fullDailyHr=8
+dailyWage=$(($wagePerHr*$fullDailyHr*$attendance))
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is Absent"
 else
 	echo "Employee is Present"
 fi
+echo "Daily Wage= "$dailyWage
