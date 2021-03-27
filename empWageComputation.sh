12f4600fbbef9683678a6921402f22e025e91bbd
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-27 15:07:10 +0530

    Added: 100 Hours Or 20 Days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,21 +5,34 @@ partTimeHr=4
 dailyWage=0
 daysinMonth=20
 wagePerMonth=0
+noOfDays=1
+noOfHours=0
+partTimeCount=0
 
-for((i=1;i<=20;i++))
-do
-	attendance=$(($RANDOM%3))
-	case $attendance in
-					0)
-						dailyWage=0
-						;;
-					1)
-						dailyWage=$(($wagePerHr*$partTimeHr))
-						;;
-					2)
-						dailyWage=$(($wagePerHr*$fullDailyHr))
-						;;
+while [ $noOfDays -lt 20 ] && [ $noOfHours -lt 100 ]
+	do
+		attendance=$(($RANDOM%3))
+		case $attendance in
+			0)
+				dailyWage=0
+				;;
+			1)
+				dailyWage=$(($wagePerHr*$partTimeHr))
+				noOfHours=$(($noOfHours+4))
+				partTimeCount=$(($partTimeCOunt+1))
+				if [ $partTimeCount -eq 2 ]
+				then
+					partTimeCount=0
+					noOfDays=$(($noOfDays+1))
+				fi
+				;;
+			2)
+				dailyWage=$(($wagePerHr*$fullDailyHr))
+				noOfHours=$(($noOfHours+8))
+				noOfDays=$(($noOfDays+1))
+				;;
 	esac
 	wagePerMonth=$(($wagePerMonth+$dailyWage))
 done
-echo "Monthly Wage= "$wagePerMonth
+
+echo "No of Hours= "$noOfHours", No of Days= "$noOfDays", Monthly Wage= "$wagePerMonth
