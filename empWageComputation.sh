a96d92302a86614da0f992615000c9f860c218e8
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-27 14:02:42 +0530

    Added: Part Time Emplyee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,19 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-attendance=$(($RANDOM%2))
+attendance=$(($RANDOM%3))
 wagePerHr=20
 fullDailyHr=8
-dailyWage=$(($wagePerHr*$fullDailyHr*$attendance))
+partTimeHr=4
+dailyWage=0
 if [ $attendance -eq 0 ]
 then
 	echo "Employee is Absent"
+	dailyWage=0
+elif [ $attendance -eq 1 ]
+then
+	echo "Employee is Present Half Day "
+	dailyWage=$(($wagePerHr*$partTimeHr))
 else
-	echo "Employee is Present"
+	echo "Employee is Present Full Day"
+	dailyWage=$(($wagePerHr*$fullDailyHr))
 fi
 echo "Daily Wage= "$dailyWage
