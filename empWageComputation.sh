4c0d3c368fbc2dcd78db254b2f05eba8e4c4cf3d
saranamsairakesh <saranamsairakesh@gmail.com>
2021-03-27 14:26:33 +0530

    Added: Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,25 @@
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-attendance=$(($RANDOM%3))
 wagePerHr=20
 fullDailyHr=8
 partTimeHr=4
 dailyWage=0
+daysinMonth=20
+wagePerMonth=0
 
-case $attendance in
-				0)
-					echo "Employee is Absent"
-					;;
-				1)
-					echo "Employee is Present Half Day"
-					dailyWage=$(($wagePerHr*$partTimeHr))
-					;;
-				2)
-					echo "Employee is Present Full Day"
-					dailyWage=$(($wagePerHr*$fullDailyHr))
-					;;
-esac
-
-echo "Daily Wage= "$dailyWage
+for((i=1;i<=20;i++))
+do
+	attendance=$(($RANDOM%3))
+	case $attendance in
+					0)
+						dailyWage=0
+						;;
+					1)
+						dailyWage=$(($wagePerHr*$partTimeHr))
+						;;
+					2)
+						dailyWage=$(($wagePerHr*$fullDailyHr))
+						;;
+	esac
+	wagePerMonth=$(($wagePerMonth+$dailyWage))
+done
+echo "Monthly Wage= "$wagePerMonth
